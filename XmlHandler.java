2f03c30ac24c702cf89e0b5282d9eef5040ce82e
karan dabas <karan15141@iiitd.ac.in>
2016-10-18 22:50:28 +0530

    2nd Commit

---
+++ XmlHandler.java
@@ -0,0 +1,32 @@
+import org.xml.sax.Attributes; //org.xml.sax package defines all the interfaces used for the SAX parser
+import org.xml.sax.helpers.DefaultHandler; // DefaultHandler class that will handle the SAX events that the parser generates
+import org.xml.sax.SAXException;
+public class XmlHandler extends DefaultHandler{
+	public void startDocument(){
+		System.out.println("Begin parsing Xml file");
+	}
+	public void startElement(String uri, String localName, String qname, Attributes attributes ) throws SAXException{
+		try{
+			System.out.println("start of : " + qname);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	public void character(char [] chArray , int start , int length){
+		System.out.println(new String(chArray,start,length));
+		}
+	}
+	public void endElement(String uri, String localName, String qname)throws SAXException{
+		try{
+			System.out.println("end of : " + qname);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	public void endDocument(){
+		System.out.println("End parsing Xml file");
+	}
+
+}
