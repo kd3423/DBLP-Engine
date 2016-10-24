58c17f3043d6343101efc260deaf7b369ff58185
kshitijqwerty <kshitijqwerty@gmail.com>
2016-10-24 07:44:25 +0530

    GUI_3rd

---
+++ Query2_GUI.java
@@ -0,0 +1,32 @@
+import java.awt.Font;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+public class Query2_GUI {
+	JPanel query2_panel = new JPanel(null);
+	JLabel publication = new JLabel("No of Publications");
+	JTextField text_publication = new JTextField();
+	public Query2_GUI(JPanel panel){
+		query2_panel.setVisible(false);
+		query2_panel.setBounds(0,125,300,500);
+		publication.setBounds(30,65,175,30);
+		publication.setFont(new Font("Courier New", Font.PLAIN, 15));
+		text_publication.setBounds(170,65,70,30);
+
+		query2_panel.add(publication);
+		query2_panel.add(text_publication);
+		panel.add(query2_panel);
+		panel.repaint();
+	}
+	public void query2_remove(){
+		query2_panel.setVisible(false);
+	}
+	public void query2_add(){
+		query2_panel.setVisible(true);
+	}
+	
+}
