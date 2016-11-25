5af508d08fb99a0654029d1fd6436698dc907660
kshitijqwerty <kshitijqwerty@gmail.com>
2016-11-25 22:17:00 +0530

    All Sorting Working for author

--- Query1_GUI.java
+++ Query1_GUI.java
@@ -1,13 +1,21 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
-
 public class Query1_GUI{
 	private JPanel query1_panel = new JPanel(null);
 	private JComboBox<String> drop = new JComboBox<String>();
@@ -15,18 +23,15 @@ public class Query1_GUI{
 	private JLabel year = new JLabel("Since Year");
 	private JLabel range = new JLabel("Custom Range            -");
 	private JTextField text_name = new JTextField();
-	private JTextField text_year = new JTextField();
-	private JTextField text_range1 = new JTextField();
-	private JTextField text_range2 = new JTextField();
+	private JTextField text_year = new JFormattedTextField();
+	private JTextField text_range1 = new JFormattedTextField();
+	private JTextField text_range2 = new JFormattedTextField();
 	private JRadioButton sort_date= new JRadioButton("Sort by Date");
-	private JRadioButton sort_year = new JRadioButton("Sort by Year");
+	private JRadioButton sort_year = new JRadioButton("Sort by Since Year");
 	private JRadioButton sort_relevence = new JRadioButton("Sort by Relevence");
 	private JRadioButton sort_bt_year = new JRadioButton("Sort in between two years");
 	private ButtonGroup buttons = new ButtonGroup();
-	
-	
 	public Query1_GUI(JPanel panel){
-		
 		query1_panel.setVisible(false);
 		query1_panel.setBounds(0,125,250,270);
 		drop.setBounds(25,20,175,30);
@@ -42,19 +47,16 @@ public class Query1_GUI{
 		text_range1.setBounds(150,135,45,30);
 		text_range2.setBounds(205,135,45,30);
 		sort_date.setBounds(20,180,150,20);
-		sort_year.setBounds(20,200,150,20);
+		sort_year.setBounds(20,200,200,20);
 		sort_relevence.setBounds(20,220,200,20);
 		sort_bt_year.setBounds(20,240,220,20);	
-		
 		drop.addItem("Search by Author Name");
 		drop.addItem("Search by Title");
-		
 		buttons.add(sort_bt_year);
 		buttons.add(sort_date);
 		sort_date.setSelected(true);
 		buttons.add(sort_relevence);
 		buttons.add(sort_year);
-		
 		query1_panel.add(drop);
 		query1_panel.add(name);
 		query1_panel.add(year);
@@ -72,15 +74,73 @@ public class Query1_GUI{
 	}
 	public void query1_remove(){
 		query1_panel.setVisible(false);
-		
 	}
 	public void query1_add(){
 		query1_panel.setVisible(true);
 	}
 	public void query1_search() throws InterruptedException, IOException{
-		
-		if(!(text_name.getText().equals(""))){
-			new Search(text_name.getText());
+		int x;
+		if(!(text_name.equals(""))){
+			if(sort_year.isSelected() && !(text_year.getText().equals(""))) x = 2;
+			else if(sort_date.isSelected()) x = 1;
+			else if(sort_bt_year.isSelected() && !(text_range1.getText().equals("")) && !(text_range2.getText().equals("")))x = 4;
+			else{
+				x = 0;
+				JFrame error = new JFrame("Error Searching");
+				error.setSize(300, 100);
+				error.setVisible(true);
+				error.setLocationRelativeTo(null);
+			    error.add(new JLabel(new ImageIcon("error.png"), JLabel.CENTER));
+			}
+			if(x != 0){
+				new Search(text_name.getText());
+				Timer timer = new Timer();
+				timer.schedule(new TimerTask(){
+					@Override
+					public void run() {
+						try {
+							SortSelect(x);
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}}, 20000);
+			}
 		}
-	}	
+	}
+	private void SortSelect(int x) throws IOException{
+		BufferedReader read = new BufferedReader(new FileReader("Ref.txt"));
+		BufferedWriter write = new BufferedWriter(new FileWriter("sort.txt"));
+		String call;
+		int i = 1;
+		while((call = read.readLine())!= null){
+			String[] z = call.split("#");
+			if(x == 1){
+				write.write(i +"#"+ z[1] + "#" + z[2] + "#" + z[3] + "#" + z[4] + "#"+ z[5] + "#" + z[6] + "\n");
+				i++;
+			}
+			if(x == 2){
+				int year = Integer.parseInt(text_year.getText());
+				if(year <= Integer.parseInt(z[4])){
+					write.write(i+"#"+ z[1] + "#" + z[2] + "#" + z[3] + "#" + z[4] + "#"+ z[5] + "#" + z[6] + "\n");
+					i++;
+				}
+			}
+			if(x == 4){
+				int year1 = Integer.parseInt(text_range1.getText()),year2 = Integer.parseInt(text_range2.getText());
+				if(year1 <= Integer.parseInt(z[4]) && Integer.parseInt(z[4]) <= year2){
+					write.write(i +"#"+ z[1] + "#" + z[2] + "#" + z[3] + "#" + z[4] + "#"+ z[5] + "#" + z[6] + "\n");
+					i++;
+				}
+			}
+		}
+		read.close();
+		write.close();
+	}
+	public void query1_reset(){
+		text_name.setText("");
+		text_range1.setText("");
+		text_range2.setText("");
+		text_year.setText("");
+		sort_date.setSelected(true);
+	}
 }
