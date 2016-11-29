343de728f1bbb1527d508b2660f2feff1232f752
kshitijqwerty <kshitijqwerty@gmail.com>
2016-11-30 00:53:14 +0530

    Some changes

--- XmlHandler.java
+++ XmlHandler.java
@@ -8,6 +8,8 @@ import java.util.*;
 public class XmlHandler implements Runnable{
 	String join;
 	private ArrayList<String> author = new ArrayList<>();	
+	private HashMap<String,Integer> hash = new HashMap<String,Integer>();
+	int k;
 	public volatile int working;
 	public void findAuth(){
 		try{
@@ -21,44 +23,71 @@ public class XmlHandler implements Runnable{
 				String snum;
 				boolean checkCat = false,checkAuth = false,checkString = true,checkTitle = false,check = false;
 				public void startElement(String uri,String localName,String qname,Attributes att)throws SAXException{
-					if(qname.equals("author")){
+					if(qname.equals("www")){
+						checkCat = false;
+					}
+					else if(qname.equals("article")){
+						checkCat = true;
+					}
+					else if(qname.equals("author") && checkCat){
 						checkAuth = true;
 						join="";
 					}
 				}
 				public void characters(char chArray[],int start,int length)throws SAXException{
-					if(checkAuth){
+					if(checkAuth && checkCat){
 						String auth = new String(chArray,start,length);
 						join = join + auth;
 					}
 				}
 				public void endElement(String uri,String localName,String qname)throws SAXException{
-					if(qname.equals("author")){
+					if(qname.equals("author") && checkCat){
 						checkAuth = false;
 						writer(join);
+					}
+					else if(qname.equals("article")){
+						checkCat = false;
 					}
 				}
 			};
 			saxTheFile.parse("dblp.xml",defHandler);
+
+			PrintWriter write = new PrintWriter( new BufferedWriter( new FileWriter ("author1.txt") ) );
+			for(String e : hash.keySet()){
+			write.println(hash.get(e) + "#" + e);
+			write.flush();
+			}
+			
+			write.close();
 		}
 		catch(Exception e){
 			e.printStackTrace();
 		}
 		finally{
 			working = 1;
+
 		}
 	}
 	private void writer(String author){
-		try{
-			PrintWriter write = new PrintWriter( new BufferedWriter( new FileWriter ( "author1.txt",true) ) );
-			write.println(author);
-			write.flush();
-			write.close();
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-		}
+		// try{
+			//PrintWriter write = new PrintWriter( new BufferedWriter( new FileWriter ( "author1.txt",true) ) );
+			if(hash.containsKey(author)){
+				k++;
+				hash.put(author, k);
+			}
+			else{
+				k = 1;
+				hash.put(author, k);
+			}
+			
+			//write.println(author);
+			//write.flush();
+			//write.close();
+		// }
+		// catch(IOException e)
+		// {
+		// 	e.printStackTrace();
+		// }
 	}
 	@Override
 	public void run() {
